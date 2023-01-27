b55ba6163e86cfcb0a8138eec554826d5684aaaa
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-01-27 17:38:23 +0300

    button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let carsouleImages = [
 	"./images/Carsoule/carsoule-1.png",
 	"./images/Carsoule/carsoule-2.png",
 	"./images/Carsoule/carsoule-3.png",
+	"./images/Carsoule/carsoule-4.png",
 ];
 let i = 0;
 document.querySelector(".next-btn").addEventListener("click", function () {
@@ -9,8 +10,19 @@ document.querySelector(".next-btn").addEventListener("click", function () {
 
 	if (i >= 0 && i < carsouleImages.length) {
 		document.getElementById("carsoul-image").src = carsouleImages[i];
+		i++;
 	} else {
 		i = 0;
 	}
-	i++;
+});
+
+document.querySelector(".prev-btn").addEventListener("click", function () {
+	console.log("clicked");
+
+	if (i >= 0 && i < carsouleImages.length) {
+		document.getElementById("carsoul-image").src = carsouleImages[i];
+		i--;
+	} else {
+		i = 0;
+	}
 });
